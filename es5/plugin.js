994d8af56a67b88431aa195003f3c6f07041025a
Davi <dluna@tbxnet.com>
2016-05-16 17:58:45 -0300

    Videojs official generator to es6

---
+++ es5/plugin.js
@@ -0,0 +1,58 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _videoJs = require('video.js');
+
+var _videoJs2 = _interopRequireDefault(_videoJs);
+
+// Default options for the plugin.
+var defaults = {};
+
+/**
+ * Function to invoke when the player is ready.
+ *
+ * This is a great place for your plugin to initialize itself. When this
+ * function is called, the player will have its DOM and child components
+ * in place.
+ *
+ * @function onPlayerReady
+ * @param    {Player} player
+ * @param    {Object} [options={}]
+ */
+var onPlayerReady = function onPlayerReady(player, options) {
+  player.addClass('vjs-concurrence-limiter');
+};
+
+/**
+ * A video.js plugin.
+ *
+ * In the plugin function, the value of `this` is a video.js `Player`
+ * instance. You cannot rely on the player being in a "ready" state here,
+ * depending on how the plugin is invoked. This may or may not be important
+ * to you; if not, remove the wait for "ready"!
+ *
+ * @function concurrenceLimiter
+ * @param    {Object} [options={}]
+ *           An object of options left to the plugin author to define.
+ */
+var concurrenceLimiter = function concurrenceLimiter(options) {
+  var _this = this;
+
+  this.ready(function () {
+    onPlayerReady(_this, _videoJs2['default'].mergeOptions(defaults, options));
+  });
+};
+
+// Register the plugin with video.js.
+_videoJs2['default'].plugin('concurrenceLimiter', concurrenceLimiter);
+
+// Include the version number.
+concurrenceLimiter.VERSION = '__VERSION__';
+
+exports['default'] = concurrenceLimiter;
+module.exports = exports['default'];
