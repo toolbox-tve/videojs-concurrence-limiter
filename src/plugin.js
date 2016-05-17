f3c083a2d59a70e9a626b0472c75bae242e52597
Francisco R Affonso <faffonso@tbxnet.com>
2016-05-17 17:45:16 -0300

    code upgrade

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,203 @@
 import videojs from 'video.js';
 
 // Default options for the plugin.
-const defaults = {};
+const defaults = {
+  interval: 10,
+  accessurl: null,
+  updateurl: null,
+  disposeurl: null,
+  playerID: null,
+  startPosition: 0
+};
+
+/**
+ * main plugin component class
+ */
+class ConcurrentViewPlugin {
+
+  constructor(options, player) {
+    this.options = options;
+    this.player = player;
+  }
+
+  /**
+   * xhr alias
+   *
+   * @param url
+   * @param data
+   * @param cb
+     */
+  makeRequest(url, data, cb) {
+    videojs.xhr(
+      {
+        body: data ? JSON.stringify(data) : '{}',
+        url: url,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      (err, resp, body) => {
+        cb(err ? err.message || err : null, body ? JSON.parse(body) : null);
+      }
+    );
+  }
+
+  validatePlay(cb) {
+
+    this.makeRequest(
+      this.options.accessurl,
+      {
+        player: this.options.playerID
+      },
+      (error, ok) => {
+        if(error) {
+          videojs.log('concurrenceview: canplay api error', error);
+          cb(new Error(error), null);
+          return ;
+        }
+
+        if (ok && ok.success) {
+          cb(null, ok);
+
+          this.player.trigger({
+            type: 'avplayercanplay',
+            code: 1
+          });
+        } else {
+          cb(new Error('Player Auth error'), null);
+        }
+      }
+    );
+
+  }
+
+  blockPlayer(code, error, reason) {
+    code = code || 'error';
+    reason = reason || 'Has alcanzado la cantidad maxima de players activos.';
+
+    videojs.log('concurrenceview: stop player - ', reason);
+
+    this.player.trigger({
+      type: 'avplayerbloked',
+      code: code,
+      reason: reason,
+      error: error
+    });
+
+    this.player.pause();
+    this.player.dispose();
+  }
+
+
+  recoverStatus(info) {
+    if (!info.position) {
+      return;
+    }
+
+    this.player.currentTime = info.position;
+
+    this.player.on('loadedmetadata', () => this.currentTime = info.position);
+
+  }
+
+  ///////////
+
+  makeWatchdog(ok) {
+
+    let watchdog = null;
+    let options = this.options;
+    let player = this.player;
+
+    let lasTime = options.startPosition || 0;
+    let playerToken, playerID = options.playerID;
+    let loadedmetadata = false;
+
+    player.on('loadedmetadata', () => loadedmetadata = true );
+
+    player.on('timeupdate', (e) => {
+
+      if(!loadedmetadata || !this.fistSent ) {
+        this.fistSent = true;
+        return;
+      }
+
+      lasTime = Math.round(player.currentTime() || 0);
+    });
+
+    videojs.log('concurrence plugin: ok', ok);
+
+
+    var cleanUp = () => {
+      videojs.log('concurrenceview: DISPOSE', options);
+
+      if (watchdog) {
+        player.clearInterval(watchdog);
+        watchdog = false;
+
+        this.makeRequest(
+          options.disposeurl,
+          {
+            player: playerID,
+            position: lasTime,
+            token: playerToken,
+            status: 'paused'
+          },
+          function(){});
+
+      }
+    };
+
+    player.on('dispose', cleanUp);
+
+    $(window).on('beforeunload', cleanUp);
+
+    if (!watchdog) {
+
+      var wdf = () => {
+
+        player.trigger({
+          type: 'avplayerupdate',
+          playerID: playerID
+        });
+
+        this.makeRequest(
+          options.updateurl,
+          {
+            player: playerID,
+            token: playerToken,
+            position: lasTime,
+            status: player.paused() ? 'paused' : 'playing'
+          },
+          function (error, ok) {
+
+            if(error) {
+              videojs.log('concurrenceview: update api error', error);
+              this.blockPlayer(player, 'authapifail', {msg: error});
+              return;
+            }
+
+            if (ok && ok.success) {
+              playerID = ok.player || playerID;
+              playerToken = ok.token || playerToken;
+
+            } else {
+              videojs.log(new Error('Player Auth error'), ok);
+              this.blockPlayer(player, 'noauth', ok);
+            }
+          }
+        );
+      };
+
+      watchdog = player.setInterval(wdf, options.interval * 1000);
+      wdf();
+    }
+
+  }
+
+  ///////////
+}
+
 
 /**
  * Function to invoke when the player is ready.
@@ -16,6 +212,26 @@ const defaults = {};
  */
 const onPlayerReady = (player, options) => {
   player.addClass('vjs-concurrence-limiter');
+
+  player._cvPlugin = new ConcurrentViewPlugin(options, player);
+  let cvPlugin = player._cvPlugin;
+
+  cvPlugin.validatePlay( function (error, ok) {
+
+    if (error) {
+      videojs.log('concurrenceview: error', error);
+      cvPlugin.blockPlayer( 'cantplay', error);
+
+    } else {
+
+      cvPlugin.recoverStatus(ok);
+      //monitor
+      cvPlugin.makeWatchdog(ok);
+    }
+
+  });
+
+
 };
 
 /**
@@ -30,9 +246,29 @@ const onPlayerReady = (player, options) => {
  * @param    {Object} [options={}]
  *           An object of options left to the plugin author to define.
  */
-const concurrenceLimiter = function(options) {
+const concurrenceLimiter = function(useroptions) {
+
+  let options = videojs.mergeOptions(defaults, useroptions);
+
+  videojs.log('concurrenceview plugin', options);
+
+  if (!options.accessurl || !options.updateurl || !options.disposeurl) {
+    videojs.log('concurrenceview: invalid urls', options);
+    return ;
+  }
+
+  if (!options.interval || options.interval < 5) {
+    videojs.log('concurrenceview: invalid options', options);
+    return ;
+  }
+
+  if (!$) {
+    videojs.log('concurrenceview: invalid jquery', options);
+    return ;
+  }
+
   this.ready(() => {
-    onPlayerReady(this, videojs.mergeOptions(defaults, options));
+    onPlayerReady(this,  options);
   });
 };
 
@@ -40,6 +276,6 @@ const concurrenceLimiter = function(options) {
 videojs.plugin('concurrenceLimiter', concurrenceLimiter);
 
 // Include the version number.
-concurrenceLimiter.VERSION = '__VERSION__';
+concurrenceLimiter.VERSION = '0.1.3';
 
 export default concurrenceLimiter;
